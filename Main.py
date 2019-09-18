eae639d6b30ca9bf5ef5cbd8eacec8aee8379b04
Rpsmith77 <rpsmith77@gmail.com>
2019-09-18 15:48:58 -0400

    test

--- Main.py
+++ Main.py
@@ -2,11 +2,29 @@
 
 #Harry Potter Sorting Hat Quiz
 
+#declaring house type variables
+gryffindor = 0
+slytherin = 0
+ravenclaw = 0
+hufflepuff = 0
+
 print("Welcome")
 
-#variables to show print platform 9 and 3/4 in a long way
+#variables to show print platform 9 and 3/4 in a long way.
 platform9 = str(3 ** 2)
 platform3 = str(10 // 3)
 platform4 = str(9 % 5)
 
 print("The train will leave from Platform", platform9 , "and", platform3 + "/" + platform4)
+print()
+
+#Intro/Instructions
+print("This Quiz will tell you which Hogwarts house you belong in.")
+print("It will tell you your next closest matching house, and the percentage you are of each house.")
+print()
+print("-----INSTRUCTIONS-----")
+print("* Answer each question with the corresponding number.")
+print("* A, B, C, D, a, b, c, and d will not work")
+print("* That is it... it's pretty simple.")
+
+
