29301c4fc2adf0373f0614a3fe4be51170625e6f
Rpsmith77 <rpsmith77@gmail.com>
2019-09-19 12:38:24 -0400

    Formating and adding house calculations.

--- Main.py
+++ Main.py
@@ -1,33 +1,15 @@
-#Ryan Smith
+# Ryan Smith
 
-#Harry Potter Sorting Hat Quiz
+# Harry Potter Sorting Hat Quiz
 
-#declaring house type variables
+# declaring house type variables
 gryffindor = 0
 slytherin = 0
 ravenclaw = 0
 hufflepuff = 0
 
-print("Welcome")
 
-#variables to show print platform 9 and 3/4 in a long way.
-platform9 = str(3 ** 2)
-platform3 = str(10 // 3)
-platform4 = str(9 % 5)
-
-print("The train will leave from Platform", platform9 , "and", platform3 + "/" + platform4)
-print()
-
-#Intro/Instructions
-print("This Quiz will tell you which Hogwarts house you belong in.")
-print("It will tell you your next closest matching house, and the percentage you are of each house.")
-print()
-print("-----INSTRUCTIONS-----")
-print("* Answer each question with the corresponding number.")
-print("* A, B, C, D, a, b, c, and d will not work")
-print("* That is it... it's pretty simple.")
-
-#This function takes the users input and converts adds to the corresponding house's score
+# This function takes the users input and converts adds to the corresponding house's score
 def answerSelect(answer):
     global gryffindor
     global slytherin
@@ -41,28 +23,54 @@ def answerSelect(answer):
         ravenclaw = ravenclaw + 1
     elif answer == 4:
         hufflepuff = hufflepuff + 1
-    
-#cite: http://www.newthinktank.com/2016/07/learn-program-5/
+
+
+# cite: http://www.newthinktank.com/2016/07/learn-program-5/
 # # This tutorial taught me how to use functions in python
 
-#Question 1
-#While loop checks if valid input.
+# Intro/Instructions
+print("Welcome Witches and Wizards!")
+print("This Quiz will tell you which Hogwarts house you belong in.")
+print("It will tell you your next closest matching house, and the percentage you are of each house.")
+print()
+print("-----INSTRUCTIONS-----")
+print("* Answer each question with the corresponding number.")
+print("* A, B, C, D, a, b, c, and d will not work")
+print("* That is it... it's pretty simple.")
+
+# Question 1
+# While loop checks if valid input.
 while True:
     try:
         print("Question 1: \nTest")
         ans = int(input())
         answerSelect(ans)
+
         if ans > 4 or ans < 1:
             print("Invalid Input. Try Again.")
             continue
+
         break
+
     except ValueError:
         print("Invalid Input. Try Again.")
+
     except:
         print("Unforeseen Error Occurred. Try Again. ")
-#Cite: http://www.newthinktank.com/2016/06/learn-program-3/
+# Cite: http://www.newthinktank.com/2016/06/learn-program-3/
 # # This tutorial taught me how to use a while loop to check for proper input
+
+# Testing
 print("Gryffindor:", gryffindor)
 print("Slytherin:", slytherin)
 print("Ravenclaw:", ravenclaw)
 print("Hufflepuff:", hufflepuff)
+
+# calculate which house you are in
+totalScore = gryffindor + slytherin + ravenclaw + hufflepuff
+
+percentGryff = gryffindor / totalScore * 100
+percentSlyth = slytherin / totalScore * 100
+percentRaven = ravenclaw / totalScore * 100
+percentHuffle = hufflepuff / totalScore * 100
+
