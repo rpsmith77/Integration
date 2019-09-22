3212d9521b625a99f0d2f2bb68a54e85beb10c3f
Rpsmith77 <rpsmith77@gmail.com>
2019-09-22 00:17:42 -0400

    Harry Potter Sorting Hat Quiz

--- Main.py
+++ Main.py
@@ -14,33 +14,17 @@ houseDescList = [line.rstrip('\n') for line in open("House_Desc.txt")]
 # Contains Intro and Instructions
 intro = [line.rstrip('\n') for line in open("Intro_Instructions.txt")]
 
-# declaring house variables
-gryffindor = 0
-slytherin = 0
-ravenclaw = 0
-hufflepuff = 0
 
-# This function takes the users input and converts adds to the corresponding house's score
-# cite: http://www.newthinktank.com/2016/07/learn-program-5/
-# # This tutorial taught me how to use functions in python
-def answerSelect(answer):
-    global gryffindor
-    global slytherin
-    global ravenclaw
-    global hufflepuff
-    if answer == 1:
-        gryffindor += 1
-    elif answer == 2:
-        slytherin += 1
-    elif answer == 3:
-        ravenclaw += 1
-    elif answer == 4:
-        hufflepuff += 1
+class House:
 
-# Cite: http://www.newthinktank.com/2016/06/learn-program-3/
-# # This tutorial taught me how to use a while loop to check for proper input
-def questionInput():
-    # While loop checks if valid input.
+    def __init__(self, name="", score=0, descript="", percent=0.0):
+        self.name = name
+        self.score = score
+        self.descript = descript
+        self.percent = percent
+
+
+def question():
     while True:
         try:
             ans = input()
@@ -49,13 +33,13 @@ def questionInput():
                 continue
             elif (65 <= ord(ans) <= 68) or (97 <= ord(ans) <= 100):  # checks if 'A' - 'D' or 'a' - 'd'
                 if ans == "A" or ans == "a":
-                    ans = 1
+                    return 1
                 elif ans == "B" or ans == "b":
-                    ans = 2
+                    return 2
                 elif ans == "C" or ans == "c":
-                    ans = 3
+                    return 3
                 else:
-                    ans = 4
+                    return 4
 
             ans = int(ans)
 
@@ -63,102 +47,107 @@ def questionInput():
                 print("Invalid Input. Try Again.")
                 continue
 
-            answerSelect(ans)
-            break
+            return ans
 
         except ValueError:
             print("Invalid Input. Try Again.")
 
-# Intro/Instructions
-for i in intro:
-    print(i)
 
-# Questions
-for i in range(0, len(questionList)):
-    print(questionList[i])
-    if i % 5 == 0 and i != 0:  # this prints out the full question before asking for the input
-        questionInput()
-        print()
+def sorting(house1, house2, house3, house4):
+    checkUnsort = [house1, house2, house3, house4]
+    checkSort = sorted(checkUnsort, reverse=True)
 
-# # Testing
-# print("Gryffindor:", gryffindor)
-# print("Slytherin:", slytherin)
-# print("Ravenclaw:", ravenclaw)
-# print("Hufflepuff:", hufflepuff)
+    i = 1
+    while i != 0:
 
-# Calculate the users percentage in each house
-totalScore = gryffindor + slytherin + ravenclaw + hufflepuff
-percentGryff = gryffindor / totalScore * 100
-percentSlyth = slytherin / totalScore * 100
-percentRaven = ravenclaw / totalScore * 100
-percentHuffle = hufflepuff / totalScore * 100
+        if checkSort[0] != checkSort[1]:  # Checks if there is a tie
 
-# used in below while loop to see what percentage in each house is largest for the user
-checkUnsort = [percentGryff, percentSlyth, percentRaven, percentHuffle]
-checkSort = sorted(checkUnsort, reverse=True)
+            if checkUnsort[0] == checkSort[0]:  # comparing sorted results w/ non sorted resuts
+                return 0
+            elif checkUnsort[1] == checkSort[0]:
+                return 1
+            elif checkUnsort[2] == checkSort[0]:
+                return 2
+            else:
+                return 3
 
-# This While Loop checks to see which house the user belongs in. It also checks to see if there is a tie.
-# If there is a tie it will ask a tie breaker question until there is no longer a tie.
-q = 1
-while q != 0:
-    if checkSort[0] != checkSort[1]:  # Checks if there is a tie
+        elif checkSort[0] == checkSort[1]:
+            print("You tied two for two or more houses. Choose Which House you wish to belong to!")
 
-        if checkUnsort[0] == checkSort[0]:  # Checks which house by comparing sorted results to non sorted results
-            print("You got Gryffindor!\n")
-            print(houseDescList[0])
-        elif checkUnsort[1] == checkSort[0]:
-            print("You got Slytherin!\n")
-            print(houseDescList[1])
-        elif checkUnsort[2] == checkSort[0]:
-            print("You got Ravenclaw!\n")
-            print(houseDescList[2])
+            answer = question()
+
+            if answer == 1:
+                return 5
+            elif answer == 2:
+                return 6
+            elif answer == 3:
+                return 7
+            else:
+                return 8
+        continue
+    i = 0
+
+
+def main():
+    gryffindor = House("Gryffindor", 0, houseDescList[0])
+    slytherin = House("Slytherin", 0, houseDescList[1])
+    ravenclaw = House("Ravenclaw", 0, houseDescList[2])
+    hufflepuff = House("Hufflepuff", 0, houseDescList[3])
+
+    for i in range(0, len(questionList)):
+        print(questionList[i])
+        if i % 5 == 0 and i != 0:  # this prints out the full question before asking for the input
+            answer = question()
+
+            if answer == 1:
+                gryffindor.score += 1
+            elif answer == 2:
+                slytherin.score += 1
+            elif answer == 3:
+                ravenclaw.score += 1
+            else:
+                hufflepuff.score += 1
+
+            print()
+
+    while True:
+        result = sorting(gryffindor.score, slytherin.score, ravenclaw.score, hufflepuff.score)
+        if result > 4:  # If it was tied this adds to the score
+
+            if result == 5:
+                gryffindor.score += 1
+            elif result == 6:
+                slytherin.score += 1
+            elif result == 7:
+                ravenclaw.score += 1
+            else:
+                hufflepuff.score += 1
         else:
-            print("You got Hufflepuff!\n")
-            print(houseDescList[3])
+            if result == 0:
+                print("You were sorted into {}!".format(gryffindor.name), "\n", gryffindor.descript, sep="")
+                break
+            elif result == 1:
+                print("You were sorted into {}!".format(slytherin.name), "\n", slytherin.descript, sep="")
+                break
+            elif result == 2:
+                print("You were sorted into {}!".format(ravenclaw.name), "\n", ravenclaw.descript, sep="")
+                break
+            else:
+                print("You were sorted into {}!".format(hufflepuff.name), "\n", hufflepuff.descript, sep="")
+                break
 
-        break  # With out this break the House Description would just infinitely print
+    totalScore = gryffindor.score + slytherin.score + ravenclaw.score + hufflepuff.score
 
-    elif checkSort[0] == checkSort[1]:
-        resultsDict = {"Gryffindor: ": percentGryff,  # Shows the results to the user so they know which houses are tied
-                       "Slytherin:  ": percentSlyth,
-                       "Ravenclaw:  ": percentRaven,
-                       "Hufflepuff: ": percentHuffle}
+    gryffindor.percent = gryffindor.score / totalScore * 100
+    slytherin.percent = slytherin.score / totalScore * 100
+    ravenclaw.percent = ravenclaw.score / totalScore * 100
+    hufflepuff.percent = hufflepuff.score / totalScore * 100
 
-        print("\nResults:")
+    print(gryffindor.name, "{:.2f}%".format(gryffindor.percent))
+    print(slytherin.name, "{:.2f}%".format(slytherin.percent))
+    print(ravenclaw.name, "{:.2f}%".format(ravenclaw.percent))
+    print(hufflepuff.name, "{:.2f}%".format(hufflepuff.percent))
 
-        for x, y in resultsDict.items():
-            print(x, "{:,.2f}%".format(y))
 
-        print("\nTie Breaker Question:\nWhich house do you want to be in?\n   1. Gryffindor\n   2. Slytherin\n   3. "
-              "Ravenclaw\n   4. Hufflepuff")
+main()
 
-        questionInput()
-        print()
-
-        totalScore = gryffindor + slytherin + ravenclaw + hufflepuff
-        percentGryff = gryffindor / totalScore * 100
-        percentSlyth = slytherin / totalScore * 100
-        percentRaven = ravenclaw / totalScore * 100
-        percentHuffle = hufflepuff / totalScore * 100
-
-        checkUnsort = [percentGryff, percentSlyth, percentRaven, percentHuffle]
-        checkSort = sorted(checkUnsort, reverse=True)
-
-        continue  # If their is a tie this will loop the tiebreaker question until there is no tie
-q = 0
-
-# # Testing
-# print("Gryffindor:", gryffindor)
-# print("Slytherin:", slytherin)
-# print("Ravenclaw:", ravenclaw)
-# print("Hufflepuff:", hufflepuff)
-
-resultsDict = {"Gryffindor: ": percentGryff,
-               "Slytherin:  ": percentSlyth,
-               "Ravenclaw:  ": percentRaven,
-               "Hufflepuff: ": percentHuffle}
-
-print("\nFinal Results:")
-
-for x, y in resultsDict.items():
-    print(x, "{:,.2f}%".format(y))
